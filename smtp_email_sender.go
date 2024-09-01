a812ef5c92559302561472de758a3afb5158b5e0
bay <30617751+bay0@users.noreply.github.com>
2024-09-01 09:34:17 +0200

    ✨ add option allowUnencrypted for dev purposes

--- smtp_email_sender.go
+++ smtp_email_sender.go
@@ -1,38 +1,51 @@
 package emailauth
 
 import (
+	"fmt"
 	"net/smtp"
 )
 
 type SMTPEmailSender struct {
-	smtpHost     string
-	smtpPort     string
-	smtpUsername string
-	smtpPassword string
-	senderEmail  string
+	smtpHost         string
+	smtpPort         string
+	smtpUsername     string
+	smtpPassword     string
+	senderEmail      string
+	allowUnencrypted bool
 }
 
-func NewSMTPEmailSender(smtpHost, smtpPort, smtpUsername, smtpPassword, senderEmail string) *SMTPEmailSender {
+func NewSMTPEmailSender(smtpHost, smtpPort, smtpUsername, smtpPassword, senderEmail string, allowUnencrypted bool) *SMTPEmailSender {
 	return &SMTPEmailSender{
-		smtpHost:     smtpHost,
-		smtpPort:     smtpPort,
-		smtpUsername: smtpUsername,
-		smtpPassword: smtpPassword,
-		senderEmail:  senderEmail,
+		smtpHost:         smtpHost,
+		smtpPort:         smtpPort,
+		smtpUsername:     smtpUsername,
+		smtpPassword:     smtpPassword,
+		senderEmail:      senderEmail,
+		allowUnencrypted: allowUnencrypted,
 	}
 }
 
 func (s *SMTPEmailSender) SendEmail(to, subject, body string) error {
-	auth := smtp.PlainAuth("", s.smtpUsername, s.smtpPassword, s.smtpHost)
+	msg := []byte(fmt.Sprintf("To: %s\r\nSubject: %s\r\n\r\n%s", to, subject, body))
 
-	msg := []byte("To: " + to + "\r\n" +
-		"Subject: " + subject + "\r\n" +
-		"\r\n" +
-		body + "\r\n")
+	smtpAddr := fmt.Sprintf("%s:%s", s.smtpHost, s.smtpPort)
 
-	err := smtp.SendMail(s.smtpHost+":"+s.smtpPort, auth, s.senderEmail, []string{to}, msg)
+	var auth smtp.Auth
+	if !s.allowUnencrypted {
+		auth = smtp.PlainAuth("", s.smtpUsername, s.smtpPassword, s.smtpHost)
+	}
+
+	var err error
+	if s.allowUnencrypted {
+		// For unencrypted connections (like MailHog in development)
+		err = smtp.SendMail(smtpAddr, nil, s.senderEmail, []string{to}, msg)
+	} else {
+		// For encrypted connections (production use)
+		err = smtp.SendMail(smtpAddr, auth, s.senderEmail, []string{to}, msg)
+	}
+
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to send email: %w", err)
 	}
 
 	return nil
